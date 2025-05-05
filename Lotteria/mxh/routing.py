cd67fd0aea25551a613d973c0ceba34d987d5d3d
Bingo <155669562+KimThuong1510@users.noreply.github.com>
2025-05-05 23:46:18 +0700

    Delete Lotteria directory

--- Lotteria/mxh/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<username>\w+)/(?P<other_username>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
